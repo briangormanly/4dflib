ea54aa317c28127f2097feaac1f4db27bfa2bc1b
Brian Gormanly <brian.gormanly@marist.edu>
2015-12-28 10:07:51 -0500

    added GenericService Comment

--- src/main/java/com/fdflib/service/GenericService.java
+++ src/main/java/com/fdflib/service/GenericService.java
@@ -5,5 +5,9 @@ import com.fdflib.service.impl.FdfCommonServices;
 /**
  * Created by brian.gormanly on 12/28/15.
  */
+
+/**
+ * Convenience service available to instantiate and use the Common Services from your controllers.
+ */
 public class GenericService implements FdfCommonServices {
 }
