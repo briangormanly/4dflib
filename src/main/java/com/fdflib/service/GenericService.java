96d4db04aa6f8ea9d25d9b888fee8feb3ebc5815
Brian Gormanly <brian.gormanly@marist.edu>
2015-12-28 10:06:08 -0500

    added GenericService

---
+++ src/main/java/com/fdflib/service/GenericService.java
@@ -0,0 +1,9 @@
+package com.fdflib.service;
+
+import com.fdflib.service.impl.FdfCommonServices;
+
+/**
+ * Created by brian.gormanly on 12/28/15.
+ */
+public class GenericService implements FdfCommonServices {
+}
